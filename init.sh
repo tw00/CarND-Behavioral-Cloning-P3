857930460626a04c76292ef9d2bc5b1b7bcb93ba
Thomas Weustenfeld <email@thomas-weustenfeld.de>
2017-01-27 06:27:45 +0000

    memory issues

--- init.sh
+++ init.sh
@@ -4,3 +4,10 @@
 sudo mkdir -p /mnt/data
 sudo chown carnd.carnd /mnt/data
 # rm -r /mnt/data/*.p
+
+sudo dd if=/dev/zero of=/mnt/swapfile.img bs=1024 count=16M
+sudo chmod 0600 /mnt/swapfile.img 
+sudo mkswap /mnt/swapfile.img 
+sudo swapon /mnt/swapfile.img
+cat /proc/swaps 
+free -m 
