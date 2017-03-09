cc03421e8c142628701fe427ef7666f4ae8e1722
Thomas Weustenfeld <email@thomas-weustenfeld.de>
2017-03-09 18:06:51 +0000

    nvidia model

--- self_driving_car/model.py
+++ self_driving_car/model.py
@@ -13,7 +13,7 @@ tf.python.control_flow_ops = tf
 # Build a model
 import keras.backend as K
 from keras.models import Sequential
-from keras.layers import Dense, Dropout, Flatten, Lambda, ELU
+from keras.layers import Dense, Dropout, Flatten, Lambda, ELU, SpatialDropout2D
 from keras.layers.core import Activation
 from keras.layers.convolutional import Convolution2D, Cropping2D
 from keras.layers.pooling import MaxPooling2D
@@ -59,6 +59,10 @@ class SDRegressionModel():
         elif model == "udacity":
             return {'name': "udacity",
                     'model': SDRegressionModel.model_udacity(),
+                    'normalizer': SDRegressionModel.no_normalize}
+        elif model == "nvidia":
+            return {'name': "nvidia",
+                    'model': SDRegressionModel.model_nvidia(),
                     'normalizer': SDRegressionModel.no_normalize}
         else: print("model %s not found" % model);
 
@@ -303,6 +307,42 @@ class SDRegressionModel():
         model.add(Dense(1))
 
         model.compile(optimizer="adam", loss="mse")
+        return model
+
+    # ----------------------------------------------------------------------------------------
+    def model_nvidia():
+        """Model based on: http://images.nvidia.com/content/tegra/automotive/images/2016/solutions/pdf/end-to-end-dl-using-px.pdf
+        """
+        def resize_images(img):
+            """Returns resized image
+            Cannot be directly used in lambda function
+            as tf is not understood by keras
+            """
+            import tensorflow as tf
+            return tf.image.resize_images(img, (66, 200))
+
+        model = Sequential()
+        # model.add(Lambda(resize_images, input_shape=input_shape))
+        model.add(Cropping2D(cropping=((45,15),(0,0)), input_shape=(192,192,3)))
+        model.add(Lambda(lambda x: x/255.-0.5))
+        model.add(Convolution2D(24, 5, 5, border_mode="same", subsample=(2,2), activation="elu"))
+        model.add(SpatialDropout2D(0.2))
+        model.add(Convolution2D(36, 5, 5, border_mode="same", subsample=(2,2), activation="elu"))
+        model.add(SpatialDropout2D(0.2))
+        model.add(Convolution2D(48, 5, 5, border_mode="valid", subsample=(2,2), activation="elu"))
+        model.add(SpatialDropout2D(0.2))
+        model.add(Convolution2D(64, 3, 3, border_mode="valid", activation="elu"))
+        model.add(SpatialDropout2D(0.2))
+        model.add(Convolution2D(64, 3, 3, border_mode="valid", activation="elu"))
+        model.add(SpatialDropout2D(0.2))
+        model.add(Flatten())
+        model.add(Dropout(0.5))
+        model.add(Dense(100, activation="elu"))
+        model.add(Dense(50, activation="elu"))
+        model.add(Dense(10, activation="elu"))
+        model.add(Dropout(0.5))
+        model.add(Dense(1))
+        model.compile(optimizer=Adam(lr=0.001), loss='mse')
         return model
 
     # ----------------------------------------------------------------------------------------
